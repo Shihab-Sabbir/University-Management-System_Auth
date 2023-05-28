c2652a27889426055424f297fe107f96271ea274
shihab-sabbir <shihab11231@gmail.com>
2023-05-29 00:15:21 +0600

    setup cors express mongoose parser [Done]

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+import express, { Application, Request, Response } from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+
+const app: Application = express();
+
+app.use(cors());
+
+//parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("University Management Auth Server Running !");
+});
+
+export default app;
